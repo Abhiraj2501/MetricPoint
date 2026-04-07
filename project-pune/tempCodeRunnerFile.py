ee8152c97c01061f5f93416dab817e3d09552771
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-04-07 20:55:13 +0530

    auto: 2026-04-07 20:55:13

--- project-pune/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-# from sklearn.preprocessing import OrdinalEncoder  # Uncomment if you prefer ordinal
-
-# 1. Load the data
