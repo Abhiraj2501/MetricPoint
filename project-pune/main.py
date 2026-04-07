14818ef34507544117d624cfdfd9ccf6768a05bc
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-04-07 20:52:28 +0530

    auto: 2026-04-07 20:52:28

--- project-pune/main.py
+++ project-pune/main.py
@@ -65,7 +65,7 @@ print(housing_prepared.shape)
 # Linear Regression
 lin_reg = LinearRegression()
 lin_reg.fit(housing_prepared, housing_labels)
-
+lin_preds = lin_reg.predict(housing_prepared)
 lin_rmse = mean_squared_error(housing_labels, lin_preds)
 
 
