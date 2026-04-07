add78f7b99c2490b1f690310f298f3e47a12f672
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-04-07 21:15:47 +0530

    auto: 2026-04-07 21:15:47

--- project-pune/main.py
+++ project-pune/main.py
@@ -1,13 +1,11 @@
+import os
 import pandas as pd
 import numpy as np
+import joblib
+ 
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import root_mean_squared_error
-from sklearn.model_selection import cross_val_score
-
